368edc9d9d5440d9b779fd82e26120ee94d974f1
Allen <zifeng.allen@gmail.com>
2022-07-13 02:58:00 -0400

    uploaded debugging code, having issues when the horse steps on previous visited cells

--- Projects/KnightTour/KnightTour.js
+++ Projects/KnightTour/KnightTour.js
@@ -1,8 +1,8 @@
 'use strict';
 
 const Size = {
-    ROW: 8,
-    COL: 8,
+    ROW: 6,
+    COL: 6,
 };
 
 
@@ -28,6 +28,7 @@ async function main() {
     let board = new Board(container);
     window.board = board;
     await board.initialize();
+    await board.move(board.knight,board.path);
     // console.log('finished');
 
 }
@@ -40,6 +41,9 @@ function Board(container) {
     this.visited = [];
     this.knight = [Math.floor(Math.random() * this.ROW),Math.floor(Math.random() * this.COL)];
     this.icon = "<i class=\"fa-solid fa-horse\"></i>"
+    this.path = [this.knight.slice()]
+    this.futurePath = []
+
 
     this.initialize = async () => {
         for (let i = 0; i < this.ROW; i++) {
@@ -64,11 +68,80 @@ function Board(container) {
 
         this.visited[this.knight[0]][this.knight[1]]=true;
         this.cells[this.knight[0]][this.knight[1]].innerHTML = this.icon;
-        console.log(1);
 
-
-
+        // this.futurePath = await this.move(this.knight,this.path);
+        // console.log(this.futurePath)
 
     }
+
+    this.validMoves = (location,path) => {
+        let x = location[0];
+        let y = location[1];
+        let possibleMoves = [[x-1,y+2],[x-1,y-2],[x-2,y+1],[x-2,y-1],[x+1,y+2],[x+1,y-2],[x+2,y+1],[x+2,y-1]];
+        let res = [];
+        for (let i = 0; i <possibleMoves.length; i++) {
+            if (0<=possibleMoves[i][0] && possibleMoves[i][0] <this.ROW && 0<=possibleMoves[i][1] && possibleMoves[i][1] <this.COL){
+                let alreadyInPath = false;
+                for (let j = 0; j < path.length; j++) {
+                    if (possibleMoves[i][0]===path[j][0] && possibleMoves[i][1]===path[j][1]){
+                        alreadyInPath = true;
+                        break;
+                    }
+                }
+
+                if (!alreadyInPath){
+                    res.push(possibleMoves[i]);
+                }
+
+            }
+        }
+        return res;
+    }
+
+    this.isAllVisited = (path) => {
+        return path.length === this.ROW * this.COL;
+    }
+
+    this.move = async (knight,path) =>{ // next all moves [[int,int]]
+
+        for (let i = 0; i < this.ROW; i++) {
+            for (let j = 0; j < this.COL; j++){
+                this.cells[i][j].classList.remove('visited','knight')
+            }
+        }
+
+        for (let i = 0; i <path.length ; i++) {
+            this.cells[path[i][0]][path[i][1]].classList.add('visited');
+        }
+
+        this.cells[knight[0]][knight[1]].classList.add('knight');
+
+        await new Promise(r => {
+            setTimeout(r, 500)
+        });
+
+
+        if (this.isAllVisited(path)){
+            return [knight]
+        }
+
+        let moves = this.validMoves(knight,path);
+        if (moves.length ===0){
+            return null;
+        }
+
+        for (let i = 0; i < moves.length; i++) {
+            let futureMoves = this.move(moves[i],path.concat([moves[i]]));
+            if ( futureMoves !== null){
+                let res = path.concat([moves[i]]).concat(futureMoves);
+                if (res.length === this.ROW * this.COL){
+                    return moves[i].concat(futureMoves);
+                }
+            }
+        }
+
+    }
+
+
 }
 
